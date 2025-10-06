fe884467726eaaa8f9c1efb6a8052d2b43a82904
mjtodd <matthew.todd@fluentforward.com>
2025-10-06 16:53:17 +0100

    add schedule link

---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Schedule() {
+  redirect('https://outlook.office.com/bookwithme/user/b17c8b977f6e43a3b6dfe7b7770e5446@fluentforward.co/meetingtype/UNpmhJy2skqvqVJ5Ql-WRg2?anonymous&ep=mlink')
+}
